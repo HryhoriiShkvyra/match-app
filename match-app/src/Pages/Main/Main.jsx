f07ca8400494083ca325c28cd4d4c8f7add5dc43
RoboTeemo <52300830+RoboTeemo@users.noreply.github.com>
2022-11-23 20:05:56 +0200

    Design for profile done

--- match-app/src/Pages/Main/Main.jsx
+++ match-app/src/Pages/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import classes from './Main.module.css'
 import IMG1 from '../../assets/img4.jpg'
+import UserInfo from "../../Components/UserInfo/UserInfo";
 
 
 
@@ -99,68 +100,7 @@ export const Main = ({userData}) => {
 
                 { moreInfoActive
                     ?
-                        <div>
-                            <div className={classes.openedInfo}>
-                            <div className={classes.openedInfoAbout}>
-                                <div className={classes.openedInfoName}>Name 99</div>
-                                <div className={classes.openedInfoAboutUser}>
-                                    <div className={classes.openedInfoAboutUserCount}>
-                                        <i class="fa-solid fa-suitcase"></i>
-                                        wort at count
-                                    </div>
-                                    <div className={classes.openedInfoAboutUserCount}>
-                                        <i class="fa-solid fa-graduation-cap"></i>
-                                        education degree count
-                                    </div>
-                                    <div className={classes.openedInfoAboutUserCount}>
-                                        <i class="fa-solid fa-house"></i>
-                                        Lives in count
-                                    </div>
-                                    <div className={classes.openedInfoAboutUserCount}>
-                                        <i class="fa-solid fa-house"></i>
-                                        sexual type count
-                                    </div>
-                                    <div className={classes.openedInfoAboutUserCount}>
-                                        <i class="fa-solid fa-location-dot"></i>
-                                        count miles/km away
-                                    </div>
-                                </div>
-                                <div className={classes.openedInfoUserAbout}>
-                                    <div className={classes.openedInfoUserName}>
-                                        About Me
-                                    </div>
-                                    <div  className={classes.openedInfoUserDescriptionBlock}>
-                                        <div className={classes.openedInfoUserDescription}>
-                                            user about himself
-                                        </div>
-                                        <div className={classes.openedInfoUserZodiacSign}>
-                                            icon
-                                            zodiac sign
-                                            
-                                        </div>
-                                        
-                                        
-                                        
-                                    </div>
-                                    <div className={classes.openedInfoUserInterests}>
-                                        <div className={classes.openedInfoUserInterest}>
-                                            {/* icon
-                                            count */}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={classes.moreAction}>
-                            <div className={classes.shareField}>
-                                <div className={classes.shareBtn}>
-                                    <button className={classes.share}>
-                                        Share profile to your friends
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                        <UserInfo/>
                     :
                     null
                 }
